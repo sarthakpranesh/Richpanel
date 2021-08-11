2c2d586ea4cc1c100290d72f06af23674793fee8
sarthakpranesh <sarthakpranesh08@gmail.com>
2021-08-11 09:03:36 +0530

    init

--- server/server.js
+++ server/server.js
@@ -1,15 +1,42 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static("public"));
+const app = express().use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+app.post('/webhook', (req, res) => {  
+  let body = req.body;
+  if (body.object === 'page') {
+    body.entry.forEach(function(entry) {
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
+  }
 });
 
-// start express server on port 5000
-app.listen(5000, () => {
-  console.log("server started on port 5000");
+
+app.get('/webhook', (req, res) => {
+  let VERIFY_TOKEN = "sarthakpranesh"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
 });
+
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
